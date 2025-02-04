9d31f3cac9d6f5dc4cda0dd7cf1356e3cd0b11c6
Baluka0013 <baluka0013@gmail.com>
2025-02-04 18:04:56 +0100

    Remove image from LobbyPage component

--- src/pages/game/LobbyPage.tsx
+++ src/pages/game/LobbyPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LobbyPage: React.FC = () => {
     return (
         <div className='overflow-hidden'>
-            <img className='' src="/lobby.jpg" alt="" height={'960px'} />
+            
         </div>
     );
 };
