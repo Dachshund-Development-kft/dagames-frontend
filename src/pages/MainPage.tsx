aa287edffebfd0b69fff3f30a470d44d28c0fe24
Baluka0013 <baluka0013@gmail.com>
2025-02-04 15:18:33 +0100

    Center align the welcome message on the Main Page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MainPage: React.FC = () => {
     return (
         <div>
-            <h1>Welcome to the Main Page</h1>
+            <h1 className='text-center'>Welcome to the Main Page</h1>
         </div>
     );
 };
