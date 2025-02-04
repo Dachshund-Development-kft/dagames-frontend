586dee8c40f6ed08a2bce8e49ab56f8bbc812384
Baluka0013 <baluka0013@gmail.com>
2025-02-04 15:11:11 +0100

    Add routing setup with React Router in main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainPage />,
+  },
+  {
+    path: '*',
+    element: <ErrorPage />,
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <RouterProvider router={router} />
+)
