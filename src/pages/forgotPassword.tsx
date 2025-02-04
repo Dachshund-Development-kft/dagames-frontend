995c3d725aa404e2f0263ac21d486cbcaf717c68
Baluka0013 <baluka0013@gmail.com>
2025-02-04 17:54:00 +0100

    Remove unused useLocation import from ForgotPassword component

--- src/pages/forgotPassword.tsx
+++ src/pages/forgotPassword.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MdAlternateEmail } from 'react-icons/md';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const ForgotPassword: React.FC = () => {
     return (
