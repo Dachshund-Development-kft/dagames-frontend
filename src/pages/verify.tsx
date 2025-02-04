6485f6980a358c7f1c9d92ff0bc15a430032db13
Baluka0013 <baluka0013@gmail.com>
2025-02-04 17:53:46 +0100

    Remove unused useLocation import from Verify page

--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavLayout from '../components/navLayout';
 import { MdAlternateEmail } from 'react-icons/md';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const ForgotPassword: React.FC = () => {
     return (
