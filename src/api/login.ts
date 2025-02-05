f3f08f6bb518a8b813769c19d4c9241a841f7c26
devbeni <ballobeni08@gmail.com>
2025-02-05 22:00:10 +0100

    Add admin-token header to login API request for authenticated users

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,6 +1,16 @@
 import axios from 'axios';
 
 export const login = async (username: string, password: string) => {
-  const response = await axios.post('https://api.dagames.online/v1/auth/login', { username, password });
-  return response.data;
+  if (localStorage.getItem('admin-token')) {
+    const response = await axios.post('https://api.dagames.online/v1/auth/login', {
+      headers: {
+        "admin-token": `${localStorage.getItem('admin-token')}`
+      },
+      data: { username, password }
+    });
+    return response.data;
+  } else {
+    const response = await axios.post('https://api.dagames.online/v1/auth/login', { username, password });
+    return response.data;
+  }
 };
