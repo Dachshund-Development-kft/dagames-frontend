8b4fa4c4ce686809c360dd605b979dbbe3cfdb96
Baluka0013 <baluka0013@gmail.com>
2025-02-06 18:10:43 +0100

    Add SettingsPage component with user data fetching and error handling

---
+++ src/pages/game/inventoryPage.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import NavLayoutGame from '../../components/navLayoutGame';
+import { me } from '../../api/me';
+
+const SettingsPage: React.FC = () => {
+
+    useEffect(() => {
+            const fetchData = async () => {
+                try {
+                    const response = await me();
+                    console.log(response.username);
+                } catch (err) {
+                    console.error(err);
+                    localStorage.removeItem('token');
+                    window.location.href = '/';
+                }
+            };
+    
+            fetchData();
+        }, []);
+
+    return (
+        <main className='flex flex-col items-center justify-center min-h-screen bg-[#0F1015]' style={{ backgroundImage: "url(/blobs.svg)" }}>
+            <NavLayoutGame />
+            <div className='flex flex-grow items-center justify-center gap-4'>
+                <h1>settings go here</h1>
+            </div>
+        </main>
+    );
+};
+
+export default SettingsPage;
