ed8052f7e5c5d714792cac1f1dbb6470b8ec3652
asd <asd@asd.com>
2025-02-13 17:53:51 +0100

    Add LobbyLayout component and integrate with LobbyPage; implement toast notifications in ShopPage for purchase feedback

--- src/pages/game/LobbyPage.tsx
+++ src/pages/game/LobbyPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import NavLayoutGame from '../../components/navLayoutGame';
 import { me } from '../../api/me';
 import socket from '../../api/socket';
+import LobbyLayout from '../../components/lobbyLayout';
 
 const PlayPage: React.FC = () => {
     useEffect(() => {
@@ -24,6 +25,7 @@ const PlayPage: React.FC = () => {
     return (
         <main className='flex flex-col items-center justify-center min-h-screen bg-[#0F1015]' style={{ backgroundImage: "url(/blobs.svg)" }}>
             <NavLayoutGame />
+            <LobbyLayout />
             <div className='flex flex-grow items-center justify-center gap-4'>
                 <h1 className='text-3xl font-bold text-white bg-clip-text'>
                     Lobby
