29a4b012843d226830dfa6975d1991f84cebbe3c
B3ni <ballobeni08@gmail.com>
2025-02-16 19:26:24 +0100

    Update API endpoints to use POST instead of GET

--- src/api/setup.ts
+++ src/api/setup.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const setup = async (weaponid: string, characterid: string) => {
-    const response = await axios.get(`https://api.dagames.online/v1/user/@me/setup`, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }, data: { weaponItemid: weaponid, characterItemid: characterid } });
+    const response = await axios.post(`https://api.dagames.online/v1/user/@me/setup`, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }, data: { weaponItemid: weaponid, characterItemid: characterid } });
     return response;
 };
