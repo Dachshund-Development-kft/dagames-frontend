0b005d60d44ed8d7443f53a46b96f9120c4aec18
B3ni <ballobeni08@gmail.com>
2025-02-16 19:33:39 +0100

    Refactor API calls in select.ts

--- src/api/select.ts
+++ src/api/select.ts
@@ -1,11 +1,27 @@
 import axios from 'axios';
 
-export const character = async (id:string) => {
-    const response = await axios.post('https://api.dagames.online/v1/user/@me/character', { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }, data: { itemid: id } });
+export const character = async (id: string) => {
+    const response = await axios.post(
+        'https://api.dagames.online/v1/user/@me/character',
+        { itemid: id },
+        {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        }
+    );
     return response;
 };
 
-export const weapon = async (id:string) => {
-    const response = await axios.post('https://api.dagames.online/v1/user/@me/weapon', { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }, data: { weaponid: id } });
+export const weapon = async (id: string) => {
+    const response = await axios.post(
+        'https://api.dagames.online/v1/user/@me/weapon',
+        { weaponid: id },
+        {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        }
+    );
     return response;
 };
