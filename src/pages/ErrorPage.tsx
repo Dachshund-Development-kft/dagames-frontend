06e079eb5891cc1cf187b5832c30721bfecbaae0
B3ni <ballobeni08@gmail.com>
2025-02-18 16:03:14 +0100

    Refactor ErrorPage component layout

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const ErrorPage: React.FC = () => {
     return (
-        <div>
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-        </div>
+        <main className='flex min-h-screen'>
+            <div className='m-auto bg-black bg-opacity-30 p-10 rounded-lg text-white'>
+                <h1 className='font-bold text-2xl text-center pb-5'>Oops! 404</h1>
+                <p className='text-center'>Sorry, an unexpected error has occurred.</p>
+            </div>
+        </main>
     );
 };
 
