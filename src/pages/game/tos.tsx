3f5d589af4ce5b62f02e5f1b251db426295e00d7
Baluka0013 <baluka0013@gmail.com>
2025-03-08 12:54:55 +0100

    Add Terms of Service component with placeholder content

---
+++ src/pages/game/tos.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TermsOfService: React.FC = () => {
+    return (
+        <div className='flex flex-col items-center justify-center gap-4 bg-black text-white p-4 h-screen'>
+            <h1>Terms of Service</h1>
+            <p>Welcome to our Terms of Service page.</p>
+            <p>By using our website you agree that any data in the website can be saved without further notice</p>
+            <p>(sorry but im too lazy to write an actual Terms Of Service)</p>
+        </div>
+    );
+};
+
+export default TermsOfService;
