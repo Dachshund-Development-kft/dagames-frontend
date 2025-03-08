2316d0a0792c0df31e1ba2bf344722cda9216d33
Baluka0013 <baluka0013@gmail.com>
2025-03-08 11:46:53 +0100

    Refactor Loading component to use NavLayoutGame and improve layout structure

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import NavLayoutGame from './nav';
 const Loading: React.FC = () => {
     return (
-        <div className="bg-black bg-opacity-75 flex flex-col items-center justify-center min-h-screen">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
-            <span className="ml-4 text-white text-xl">Loading...</span>
-        </div>
+        <main className='flex flex-col items-center justify-center min-h-screen overflow-hidden'>
+            <NavLayoutGame />
+            <div className='flex flex-grow items-center justify-center w-full'>
+                <h1 className='text-black text-6xl font-bold'>Loading...</h1>
+            </div>
+        </main>
     );
 };
 
