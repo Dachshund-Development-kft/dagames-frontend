9c450278343468f3ba34f4b3baf731c481f342ad
B3ni <ballobeni08@gmail.com>
2025-03-11 16:47:49 +0100

    Remove unused socket connection in AlertLayout component

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { getNews } from '../api/news';
-import socket from '../api/socket';
 
 interface ApiResponse {
     maintenance: boolean;
@@ -19,7 +18,6 @@ interface ApiResponse {
 
 const AlertLayout: React.FC = () => {
     const [maintenanceMessage, setMaintenanceMessage] = useState<string | null>(null);
-    const [socketConnected, setSocketConnected] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -34,20 +32,6 @@ const AlertLayout: React.FC = () => {
             }
         };
 
-        const connectSocket = () => {
-            socket.on('connect', () => {
-                setSocketConnected(true);
-
-                socket.on('auth', (data) => {
-                    if (data.success) {
-                        console.log('Socket authenticated');
-                        setSocketConnected(true);
-                    }
-                });
-            });
-        };
-
-        connectSocket();
         fetchData();
     }, []);
 
@@ -56,12 +40,6 @@ const AlertLayout: React.FC = () => {
             {maintenanceMessage && (
                 <div className="fixed top-0 left-0 w-full bg-yellow-500 bg-opacity-30 backdrop-blur-md text-black p-4 text-center z-50">
                     {maintenanceMessage}
-                </div>
-            )}
-
-            {!socketConnected && (
-                <div className="fixed top-0 left-0 w-full bg-blue-900 bg-opacity-30 backdrop-blur-md text-black p-4 text-center z-50">
-                    Connection to server lost. Please refresh the page.
                 </div>
             )}
         </>
