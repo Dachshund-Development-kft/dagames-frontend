7475a9b2e6798e92aa930597067607beb9b6f10e
b3ni15 <ballobeni08@gmail.com>
2025-03-22 01:12:34 +0100

    Fix image source path to use lowercase character names for player in GamePage component

--- src/pages/game/gamePage.tsx
+++ src/pages/game/gamePage.tsx
@@ -249,7 +249,7 @@ const GamePage: React.FC = () => {
                     <>
                         <div className='flex justify-center items-center gap-8 mt-16'>
                             <img src={`/characters/${enemyInfo.character.name.toLowerCase()}${enemyInfo.weapon.name}.png`} alt={enemyInfo.character.name} className='w-42 h-64' style={{ transform: 'scaleX(-1)' }} />
-                            <img src={`/characters/${playerInfo.character.name}${playerInfo.weapon.name}.png`} alt={playerInfo.character.name} className='w-42 h-64' />
+                            <img src={`/characters/${playerInfo.character.name.toLowerCase()}${playerInfo.weapon.name}.png`} alt={playerInfo.character.name} className='w-42 h-64' />
                         </div>
 
                         <div className='mt-4 text-center'>
